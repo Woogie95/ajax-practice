3d216df8a20e374e7a47cbbb0c38e15bec29fb9c
Woogie95 <choisw950928@gmail.com>
2023-04-04 19:17:57 +0900

    feat: ajax post 요청 방식

--- src/main/java/com/example/ajaxpractice/controller/AjaxController.java
+++ src/main/java/com/example/ajaxpractice/controller/AjaxController.java
@@ -2,12 +2,20 @@ package com.example.ajaxpractice.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AjaxController {
     @GetMapping("/ex01")
     public String ex01() {
-        System.out.println("asdasdasd");
-        return "index";
+        System.out.println("나는 01 입니다.");
+        return "index"; // index.html 에 작성된 코드가 출력됨
+    }
+
+    @PostMapping("/ex02")
+    public @ResponseBody String ex02() {
+        System.out.println("나는 02 입니다.");
+        return "index"; // 리턴값이 그대로 출력됨 (리턴 하는 값을 Body 에 실어서 같이 보내준다.)
     }
 }
