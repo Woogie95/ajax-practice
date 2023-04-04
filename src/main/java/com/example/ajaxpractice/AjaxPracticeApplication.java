e00cc08b0fffc8a00b862465f322536771403096
Woogie95 <choisw950928@gmail.com>
2023-04-04 17:50:13 +0900

    Initial commit

---
+++ src/main/java/com/example/ajaxpractice/AjaxPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ajaxpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaxPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjaxPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ajaxpractice/AjaxPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ajaxpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjaxPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
