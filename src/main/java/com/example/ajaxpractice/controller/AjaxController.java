0b1a198391ce53e7313c5575c1406ac1377897c3
Woogie95 <choisw950928@gmail.com>
2023-04-05 16:14:51 +0900

    feat: ajax get, post 요청 & 파라미터로 보내기

--- src/main/java/com/example/ajaxpractice/controller/AjaxController.java
+++ src/main/java/com/example/ajaxpractice/controller/AjaxController.java
@@ -3,6 +3,7 @@ package com.example.ajaxpractice.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -16,6 +17,20 @@ public class AjaxController {
     @PostMapping("/ex02")
     public @ResponseBody String ex02() {
         System.out.println("나는 02 입니다.");
-        return "index"; // 리턴값이 그대로 출력됨 (리턴 하는 값을 Body 에 실어서 같이 보내준다.)
+        return "성욱"; // 리턴값이 그대로 출력됨  (리턴 하는 값을 Body 에 실어서 같이 보내준다.)
+    }
+
+    @GetMapping("/ex03")
+    public @ResponseBody String ex03(@RequestParam("param1") String param1,
+                                     @RequestParam("param2") String param2) {
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+        return "ex03 메소드 호출";
+    }
+
+    @PostMapping("/ex04")
+    public @ResponseBody String ex04(@RequestParam("param1") String param1,
+                                     @RequestParam("param2") String param2) {
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+        return "ex04 메소드 호출";
     }
 }
